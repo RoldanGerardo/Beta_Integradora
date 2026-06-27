f7cad89064f3ee5ccf0784570940c2ae27344830
Mark <markjdkahd@gmail.com>
2026-06-27 14:42:29 -0500

    Solución de configuración DE UNA CARPETA QUE NUNCA VIMOS ALABERGA JWDFNSUESHYDFUYSEBGYDFUEAUEFSKIUQWIHEJFH87WY4YGR7FIYUFQ4WHEISJDFo

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,13 +2,13 @@
    src/main.tsx
    Punto de entrada de la app BETA (React + Vite)
    ───────────────────────────────────────────────── */
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './Styles/index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
