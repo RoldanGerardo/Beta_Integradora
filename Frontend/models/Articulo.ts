0acb9d1e2329d9d801cd39f283fc1e76f9d05c15
NikAyelen <nikibts9795@gmail.com>
2026-06-30 11:55:59 -0500

    Cree los modelos de articulos , egresos , ingresos y movimientos en el front , con coherencia junto al back, con su respectivo contenido de las clases

---
+++ Frontend/models/Articulo.ts
@@ -0,0 +1,6 @@
+export interface Articulo  {
+    id?: number;
+    titulo: string;
+    contenido: string;
+    fecha: string;
+}
---
+++ Frontend/models/Movimiento.ts
@@ -0,0 +1,8 @@
+// src/models/Movimiento.ts
+
+export interface Movimiento {
+  id?: number;          
+  monto: number;       
+  descripcion: string;  
+  fecha: string;
+}
